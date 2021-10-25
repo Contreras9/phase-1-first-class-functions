fcfcd53166835aebfa091a7cc6b8353936bbe8a5
Hamzah Contreras <Hamzah_36@outlook.com>
2021-10-25 11:00:14 -0500

    Completed

---
+++ index.js
@@ -0,0 +1,24 @@
+/*function receivesAFunction(num1, num2, callback) {
+    let number = callback(num1, num2);
+    console.log(number)
+}
+
+
+receivesAFunction(5,3, (num1, num2) => num1 * num2);
+*/
+
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+
+function returnsANamedFunction() {
+    const five = function() {return 5};
+    return five;
+}
+
+function returnsAnAnonymousFunction() {
+    return function(){return 5};
+}
