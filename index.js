c43cdc50204f06398c143f55bf5ad9950ae43316
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-11 17:08:33 -0600

    commit tree 93e4c09cb9ce0145a5de8e77ac9be4996e624edb

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-/*function receivesAFunction(num1, num2, callback) {
-    let number = callback(num1, num2);
-    console.log(number)
+// /*function receivesAFunction(num1, num2, callback) {
+//     let number = callback(num1, num2);
+//     console.log(number)
+// }
+
+
+// receivesAFunction(5,3, (num1, num2) => num1 * num2);
+// */
+
+
+function receivesAFunction(cb) {
+    return cb();
 }
-
-
-receivesAFunction(5,3, (num1, num2) => num1 * num2);
-*/
-
-
-function receivesAFunction(callback) {
-    callback();
-}
-
-
 
 function returnsANamedFunction() {
-    const five = function() {return 5};
+    const five = function() {return };
     return five;
 }
 
 function returnsAnAnonymousFunction() {
-    return function(){return 5};
+    return function(){return "Hamzah"};
 }
 
 
-/*let weirdFunction = returnsAWeirdfunction();
+// /*let weirdFunction = returnsAWeirdfunction();
 
-weirdFunction(() => console.log("Hello"));
+// weirdFunction(() => console.log("Hello"));
 
-function returnsAWeirdfunction() {
-    return receivesAFunction;
-}*/
+// function returnsAWeirdfunction() {
+//     return receivesAFunction;
+// }*/
